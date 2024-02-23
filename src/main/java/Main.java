8614cee5ee260fcc4bc09c6609b0e791623246e0
X4PDK0P <xapdkopplay@gmail.com>
2024-02-24 01:00:41 +0300

    2. Добавление товаров в калькулятор Запросите у пользователя название товара и его стоимость. Стоимость должна быть в формате рубли.копейки, например 10.45 или 11.40.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,6 +16,16 @@ public class Main {
         }
     }
 
+    public static boolean isPriceDouble(String strPrice){
+        try {
+            double price = Double.parseDouble(strPrice);
+                return true;
+        } catch (NumberFormatException e) {
+            System.out.println("Введено не корректное значение, стоимость должна быть в формате {рубли.копейки}");
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
         boolean numberOfPeopleCorrectly = false;
         Scanner scanner = new Scanner(System.in);
@@ -25,6 +35,21 @@ public class Main {
             String people = scanner.nextLine();
             if (isInt(people)) {
                 numberOfPeopleCorrectly = true;
+            }
+        }
+        System.out.println("Добавление товаров в калькулятор");
+        while (true) {
+            System.out.println("Пример:\n Помидор 10.45");
+            String product = scanner.next();
+            // принимаем не известный тип данных
+            String strPrice = scanner.next();
+            if (product.equalsIgnoreCase("завершить") || strPrice.equalsIgnoreCase("завершить")) {
+                break;
+            }
+            if (isPriceDouble(strPrice)){
+                //товар корректный
+                double price = Double.parseDouble(strPrice);
+                //item(product, price); добавление товара в класс калькулятор
             }
         }
     }
