2756b03198b75939e2edb067734e635c9fc7caa2
X4PDK0P <xapdkopplay@gmail.com>
2024-02-24 00:18:40 +0300

    1. Входные параметры для счётчика После запуска программа должна спрашивать у пользователя, на скольких человек необходимо разделить счёт. Программа должна выводить сообщение об ошибке, когда: Количество человек, введённых пользователем, равно 1. В этом случае нет смысла ничего считать и делить. Количество человек меньше 1. Это некорректное значение для подсчёта. Если пользователь ввёл корректное значение > 1, то программа продолжает выполнение. Если значение некорректное, то показать ошибку и заново попросить пользователя ввести корректное количество гостей. Запрашивать количество гостей необходимо до тех пор, пока не будет введено корректное значение > 1.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
 
 public class Main {
+    public static boolean isInt(String people){
+        try {
+            int peopleInt = Integer.parseInt (people);
+            if (peopleInt <= 1){
+                System.out.println("Введено не корректное кол-во человек");
+                return false;
+            } else {
+                return true;
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Введено не корректное значение, используйте целые числа");
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean numberOfPeopleCorrectly = false;
+        Scanner scanner = new Scanner(System.in);
+        while (!numberOfPeopleCorrectly) {
+            System.out.println("на скольких человек необходимо разделить счёт?");
+            // принимаем не известный тип данных
+            String people = scanner.nextLine();
+            if (isInt(people)) {
+                numberOfPeopleCorrectly = true;
+            }
+        }
     }
 }
